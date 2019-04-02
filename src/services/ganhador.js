6820c50645be69ccd2fc3016233d0956a7481b82
Jeudi Prando <jeudiprando@gmail.com>
2019-04-02 13:09:58 -0300

    fazendo o algoritimo falar, fluent

--- src/services/ganhador.js
+++ src/services/ganhador.js
@@ -1,14 +1,16 @@
 const vitorias = [448, 56, 7, 292, 146, 73, 273, 84]
 const convertLetra = letra => value => (value == letra ? 1 : 0)
-const toBinario = (arr, convertLetra) => arr.map(convertLetra).join('')
+const toBinario = (arr, verificaLetra) => arr.map(verificaLetra).join``
 const haGanhador = valor => element => (element & valor) == element
 
-const verificaSeGanhou = (letra, partida) =>
-  vitorias.some(
-    haGanhador(parseInt(toBinario(partida, convertLetra(letra)), 2))
-  ) && letra
+const verificaSe = letra => {
+  const nessa = partida => parseInt(toBinario(partida, convertLetra(letra)), 2)
+  return {
+    ganhou: partida => vitorias.some(haGanhador(nessa(partida))) && letra
+  }
+}
 
-const ganhador = jogada =>
-  verificaSeGanhou('X', jogada) || verificaSeGanhou('O', jogada)
+const ganhador = partida =>
+  verificaSe`X`.ganhou(partida) || verificaSe`O`.ganhou(partida)
 
 export default ganhador
