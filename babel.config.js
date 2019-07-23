a6956df8072637c5bd22ddba0aa7046775700026
Jeudi Prando <jeudi@adsoft.com.br>
2019-07-23 11:35:31 -0300

    resolvendo libs com vunerabilidades e adicionando packtracker.io ao projeto

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,14 @@
+const PacktrackerPlugin = require('@packtracker/webpack-plugin')
+
 module.exports = {
   presets: [
     '@vue/app'
+  ],
+  plugins: [
+    new PacktrackerPlugin({
+      project_token: '0dfbc838-9ecd-43d3-9b6f-a582f2e4eea1',
+      upload: true,
+      fail_build: true
+    })
   ]
 }
